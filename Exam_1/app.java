98bf9c9ef5d7582e4da4d556a55041d57e775696
Ackzel John Borero <111472859+ackzel-06@users.noreply.github.com>
2022-09-05 10:08:09 +0800

    Update app.java

--- Exam_1/app.java
+++ Exam_1/app.java
@@ -4,7 +4,7 @@ public class App {
         Student Student = new Student();
         Student.surname = "Borero";
         Student.firstname = "Ackzel john";
-        Student.middleInitial = "E.";
+        Student.middleInitial = 'E';
         Student.dateOfBirth = "06/04/2002";
         Student.studentNumber = 2022101645;
         Student.studentEmailAddress = "boreroae@students.national-u.edu.public";
