0148cc2d5e3dfad84c30617a951b2403061b0334
Ackzel John Borero <111472859+ackzel-06@users.noreply.github.com>
2022-09-05 09:57:45 +0800

    Create app.java

---
+++ Exam_1/app.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Student Student = new Student();
+        Student.surname = "Borero";
+        Student.firstname = "Ackzel john";
+        Student.middleInitial = "E.";
+        Student.dateOfBirth = "06/04/2002";
+        Student.studentNumber = 2022101645;
+        Student.studentEmailAddress = "boreroae@students.national-u.edu.public";
+        Student.iAmAwesome = true;
+        Student.saymyStudentNumber();
+        Student.sayMystudentEmailAddress();
+        Student.amIAwesome();
+        }
+    }
